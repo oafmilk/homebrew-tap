8a2e0cbcfef728efe246b701e1ce6d490084a407
Ethan <50385867+ethanr-io@users.noreply.github.com>
2024-08-30 16:12:13 +1000

    Updated timezy.rb

--- Casks/timezy.rb
+++ Casks/timezy.rb
@@ -2,7 +2,7 @@ cask "timezy" do
   version "0.1.0"
   sha256 "c5426065238ec7ae156d727064966bb365c9ff72e620a6f9a753521cca1604ec"
 
-  url "https://raw.githubusercontent.com/ethanr-io/releases/HEAD/Timezy-#{version}.dmg"
+  url "https://cdn.ethanr.io/Timezy-#{version}.dmg"
   name "Timezy"
   desc "Simple timezone app"
   homepage "https://ethanr.io/"
